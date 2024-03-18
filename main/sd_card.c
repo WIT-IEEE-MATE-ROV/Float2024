a4b4784898ccaa9ad6ad6c368e3e8e3d68b9136f
clayton14 <claytoneasley77@gmail.com>
2024-03-18 19:56:44 -0400

    SD-Card setup
        create sd_card.c and sd_card.h and add to CMakeLists.txt

---
+++ main/sd_card.c
@@ -0,0 +1,13 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_system.h>
+#include <time.h>
+#include "sdmmc_cmd.h"
+
+static const char *TAG = "sd_card";
+
+void sd_card_init()
+{
+
+
+}
